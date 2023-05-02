1a6e087f176ed387c1c4ef6d3bf256cf4583b194
ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>
2023-05-02 18:19:09 +0300

    Alwaleed Khushaim star HW2
    Signed-off-by: ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>

--- star.dart
+++ star.dart
@@ -3,20 +3,13 @@ void main() {
   var a="*";
   var e="*";
   var r=4;
- 
   
   for (int i = 0; i < 8; i++) {
-    
     if (i<4){
       print (a);
-      a=a+e;
-    }else{
-      
-   var result = a.substring(4);
+      a=a+e;}
+    else
+      {var result = a.substring(4);
       result = a.substring(0,r-1);
       print(result);
-      r--;
-    }
-    
-  }
-}
+      r--; }}}
